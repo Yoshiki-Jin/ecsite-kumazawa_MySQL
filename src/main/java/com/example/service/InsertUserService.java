667f80d4cc20b2d8e7ee4eebcf9fecce31497689
saia-i <saisaismh.0896@gmail.com>
2022-11-30 20:05:52 +0900

    パスワードハッシュ化の機能を実装しました.

--- src/main/java/com/example/service/InsertUserService.java
+++ src/main/java/com/example/service/InsertUserService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +20,18 @@ public class InsertUserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	/**
 	 * ユーザー情報を登録するメソッド.
 	 * 
 	 * @param user ユーザー情報
 	 */
 	public void insert(User user) {
+
+		// パスワードをハッシュ化
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userRepository.insert(user);
 	}
 
