e485000b932a1b97e3019b5b35d145ef572b3681
ikuto24680 <siutikuto@gamil.com>
2022-11-30 09:20:27 +0900

    1-5&1-2 未ログイン時のカート表示処理

--- src/main/java/com/example/controller/CartController.java
+++ src/main/java/com/example/controller/CartController.java
@@ -1,6 +1,6 @@
 package com.example.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,41 +15,53 @@ import com.example.service.CartService;
 
 /**
  * カートに商品を追加、削除、カートの表示をするコントローラ.
+ * 
  * @author kaneko
  */
 @Controller
 @RequestMapping("/cart")
 public class CartController {
-	
+
 	@Autowired
 	private CartService service;
+	
+	@Autowired
+	private HttpSession session;
 
 	/**
 	 * 選択されたOrderItemを登録する
-	 * @param form　CartForm
-	 * @return　商品一覧ページに移動
+	 * 
+	 * @param form CartForm
+	 * @return 商品一覧ページに移動
 	 */
 	@PostMapping("insertOrderItem")
 	public String insertOrderItem(CartForm form) {
-		
-		//ユーザーIDを入手する（未実装）
+
+		// ユーザーIDを入手する（未実装）
 		Integer userId = 0;
 		service.addItem(form, userId);
 		return "item_list";
 	}
-	
+
 	/**
 	 * カートの中身を表示する
-	 * @param model　リクエストスコープ用
-	 * @return　カート詳細画面に移動
+	 * 
+	 * @param model リクエストスコープ用
+	 * @return カート詳細画面に移動
 	 */
 	@GetMapping("showCart")
 	public String showCart(Model model) {
 		
-		//ユーザーIDを入手する（未実装）
+		if(session.getAttribute("user") == null) {
+			
+			session.setAttribute("throughOrderConfirmation", true);
+			return "login";
+		}
+
+		// ユーザーIDを入手する（未実装）
 		Integer userId = 1;
 		Order order = service.showCart(userId);
-		model.addAttribute("order",order);
+		model.addAttribute("order", order);
 		return "cart_list";
 	}
 }
